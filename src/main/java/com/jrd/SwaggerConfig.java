c5a9399e9d56bcbddac5a8beb910e74701550f75
jr.domingos <domingoshot@hotmail.com>
2020-05-20 16:21:18 -0300

    condig

--- src/main/java/com/jrd/SwaggerConfig.java
+++ src/main/java/com/jrd/SwaggerConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -18,7 +19,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 @EnableSwagger2
-public class SwaggerConfig extends WebMvcConfigurationSupport  {	
+public class SwaggerConfig extends WebMvcConfigurerAdapter  {	
 	@Override
       public void addResourceHandlers(ResourceHandlerRegistry registry) {
 		    registry.addResourceHandler("swagger-ui.html")
